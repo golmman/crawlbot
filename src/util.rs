cbb4dc9a9e70b40cf4c019e7293ce63d8bc442b2
Dirk Kretschmann <kretschmanndi@gmail.com>
2019-07-04 23:15:36 +0200

    added color_print functions for printing colorful

--- src/util.rs
+++ src/util.rs
@@ -1,1 +1,2 @@
+pub mod color_print;
 pub mod json_option;
---
+++ src/util/color_print.rs
@@ -0,0 +1,40 @@
+// see https://en.wikipedia.org/wiki/ANSI_escape_code
+
+const RED: &str = "\x1B[1;31m";
+const BLUE: &str = "\x1B[1;34m";
+const CYAN: &str = "\x1B[1;36m";
+const GREEN: &str = "\x1B[0;32m";
+const RESET: &str = "\x1B[0;0m";
+
+#[allow(dead_code)]
+pub fn red(text: &str) -> String {
+    format!("{}{}{}", RED, text, RESET)
+}
+
+#[allow(dead_code)]
+pub fn blue(text: &str) -> String {
+    format!("{}{}{}", BLUE, text, RESET)
+}
+
+#[allow(dead_code)]
+pub fn cyan(text: &str) -> String {
+    format!("{}{}{}", CYAN, text, RESET)
+}
+
+#[allow(dead_code)]
+pub fn green(text: &str) -> String {
+    format!("{}{}{}", GREEN, text, RESET)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn colors() {
+        println!("Her is some {} text.", red("red colored"));
+        println!("Her is some {} text.", blue("blue colored"));
+        println!("Her is some {} text.", cyan("cyan colored"));
+        println!("Her is some {} text.", green("green colored"));
+    }
+}
